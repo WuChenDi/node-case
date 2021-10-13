91b8cf25acf7542a0ab29963f99e7d932ea56fb4
WuChenDi <996194720@qq.com>
2021-10-13 21:07:08 +0800

    docs: add code comments

--- fileupload/src/server.js
+++ fileupload/src/server.js
@@ -12,8 +12,10 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// 定义上传目录地址
 const UPLOAD_DIR = path.resolve(__dirname, 'public/upload');
 
+// 上传路由
 app.post('/upload', function (req, res) {
   const form = new multiparty.Form({ uploadDir: 'temp' });
   form.parse(req);
@@ -26,26 +28,33 @@ app.post('/upload', function (req, res) {
     }
     // 源文件名称.index.ext
     const dPath = path.join(chunkDir, chunk.originalFilename.split('.')[1]);
+    // 将切片临时目录移动到存放目录
     await fse.move(chunk.path, dPath, { overwrite: true });
 
     res.send('文件上传成功');
   });
 });
 
+// 合并分片路由
 app.post('/merge', async function (req, res) {
+  // 传递名称
   const { name } = req.body;
   const fname = name.split('.')[0];
 
+  // 获取文件名的索引分片
   const chunkDir = path.join(UPLOAD_DIR, fname);
   const chunks = await fse.readdir(chunkDir);
 
+  // 分片排序
   chunks
     .sort((a, b) => a - b)
     .map(chunkPath => {
       // 合并文件
       fs.appendFileSync(path.join(UPLOAD_DIR, name), fs.readFileSync(`${chunkDir}/${chunkPath}`));
     });
+  // 合并完成删除分片目录
   fse.removeSync(chunkDir);
+  // 返回合并成功提示与地址
   res.send({ msg: '合并成功', url: `http://localhost:5500/upload/${name}` });
 });
 
