e556ce27d5b4d64b405a7e6af52f77bc00fcb6f6
WuChenDi <996194720@qq.com>
2021-10-14 15:09:27 +0800

    docs: Supplementary code comments

--- video-stream/app.js
+++ video-stream/app.js
@@ -5,31 +5,45 @@ const videoPath = './1-1 课程导学.mp4';
 
 http
   .createServer(async (req, res) => {
+    // 定义主页路由
     if (req.url === '/') {
+      // 定义返回类型
       res.writeHead(200, { 'Content-Type': 'text/html' });
+      // 返回 video 标签
       res.end(`<video src="/video" width="500" controls="controls"></video>`);
     } else if (req.url === '/video') {
-      let range = req.headers['range'];
+      // range 表示发送范围请求，获取资源部分数据
+      const range = req.headers['range'];
       if (range) {
-        let stats = await stat(videoPath);
-        let r = range.match(/=(\d+)-(\d+)?/);
+        const stats = await stat(videoPath);
+        const r = range.match(/=(\d+)-(\d+)?/);
+        // 视频开始位置
         let start = parseInt(r[1], 10);
+        // 视频结束位置，如果不存在取默认位置一兆数据
         let end = r[2] ? parseInt(r[2], 10) : start + 1024 * 1024;
         if (end > stats.size - 1) {
           end = stats.size - 1;
         }
 
-        let head = {
+        // 设置响应头信息
+        const head = {
+          // 类型
           'Content-Type': 'video/mp4,',
+          // 区块范围以及视频长度
           'Content-Range': `bytes ${start}-${end}/${stats.size}`,
+          // 区块长度
           'Content-Length': end - start + 1,
+          // 定义范围的单位
           'Accept-Ranges': 'bytes'
         };
+        // 206表示 range 响应信息
         res.writeHead(206, head);
+        // 读取文件输出到客户端
         fs.createReadStream(videoPath, { start, end }).pipe(res);
+      } else {
+        // 读取文件输出到客户端
+        fs.createReadStream(videoPath).pipe(res);
       }
-    } else {
-      fs.createReadStream(videoPath).pipe(res);
     }
   })
   .listen(3000, () => {
